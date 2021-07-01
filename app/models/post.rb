490c1c33f145a0998ff65ba1cf4ed7f534c9b4d2
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-07-01 14:16:33 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction || Non-Fiction) }
+    validate :validates_clickbait
+
+    def validates_clickbait
+        if title.present? && !title.include?("Won't Believe")
+            errors.add(:title, "not clickbaity enough!")
+    end
+end
+
 end
